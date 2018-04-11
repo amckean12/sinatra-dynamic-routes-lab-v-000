e2af8cf5e98fd2d92d2874700fc261ac9fed5b4e
Alex McKean <amckean@me.com>
2018-04-11 15:18:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,13 +36,13 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
     result = nil
-    if operation == "add"
+    if operation == "add" 
       result = (number1 + number2).to_s
     elsif operation == "subtract"
       result = (number1 - number2).to_s
     elsif operation == "multiply"
       result = (number1 * number2).to_s
-    elsif operantion == "divide"
+    elsif operation == "divide"
       result = (number1 / number2).to_s
     else
       result = "Unable to perform this operation"
